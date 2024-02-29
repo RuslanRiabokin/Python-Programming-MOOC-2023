eeb7401d199d052d815b3036cb8372a42f8a35f9
Riabokin Ruslan <rriabokin@gmail.com>
2024-02-29 14:38:09 +0200

    Часть 9.3 Инкапсуляция

--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,19 @@
 # WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__petrol_tank = 0
+        self.__odometer_reading = 0
+
+    def fill_up(self):
+        self.__petrol_tank = 60
+
+    def drive(self, km):
+        petrol_needed = km
+        if petrol_needed > self.__petrol_tank:
+            petrol_needed = self.__petrol_tank
+        self.__odometer_reading += petrol_needed
+        self.__petrol_tank -= petrol_needed
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer_reading} km, petrol remaining {self.__petrol_tank} litres"
+
--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,22 @@
 # WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, name):
+        self.__name = name
+        self.__observations = []
+
+    def add_observation(self, observation):
+        self.__observations.append(observation)
+
+    def latest_observation(self):
+        if self.__observations:
+            return self.__observations[-1]
+        else:
+            return ""
+
+    def number_of_observations(self):
+        return len(self.__observations)
+
+    def __str__(self):
+        return f"{self.__name}, {self.number_of_observations()} observations"
 
 
