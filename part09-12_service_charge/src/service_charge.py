f6b936baa8fd25d9d88a5df87b5759378f8080c5
Riabokin Ruslan <rriabokin@gmail.com>
2024-03-01 10:56:05 +0200

    Часть 9.4 Область применения методов

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,25 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, owner: str, account_number: str, balance: float):
+        self.__owner = owner
+        self.__account_number = account_number
+        self.__balance = balance
+
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+
+    def withdraw(self, amount: float):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            self.__service_charge()
+
+    def __service_charge(self):
+        self.__balance -= self.__balance * 0.01
+
+    @property
+    def balance(self):
+        return self.__balance
 
 
