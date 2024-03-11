ef2fd6b02c1db02f6d601a8c7f1d57207f16f342
Riabokin Ruslan <rriabokin@gmail.com>
2024-03-11 18:08:56 +0200

    Часть 10 Закончил

--- part10-11_phone_book_v2/src/phone_book_v2.py
+++ part10-11_phone_book_v2/src/phone_book_v2.py
@@ -1,18 +1,40 @@
+class Person:
+    def __init__(self, name):
+        self.__name = name
+        self.__numbers = []
+        self.__address = None
+    
+    def name(self):
+        return self.__name
+    
+    def add_number(self, number):
+        self.__numbers.append(number)
+    
+    def numbers(self):
+        return self.__numbers
+    
+    def add_address(self, address):
+        self.__address = address
+    
+    def address(self):
+        return self.__address
 
-# Write your solution here:
 class PhoneBook:
     def __init__(self):
         self.__persons = {}
 
     def add_number(self, name: str, number: str):
-        if not name in self.__persons:
-            self.__persons[name] = []
-        self.__persons[name].append(number)
+        if name not in self.__persons:
+            self.__persons[name] = Person(name)
+        self.__persons[name].add_number(number)
+
+    def add_address(self, name: str, address: str):
+        if name not in self.__persons:
+            self.__persons[name] = Person(name)
+        self.__persons[name].add_address(address)
 
     def get_entry(self, name: str):
-        if not name in self.__persons:
-            return None
-        return self.__persons[name]
+        return self.__persons.get(name)
 
     def all_entries(self):
         return self.__persons
@@ -26,20 +48,35 @@ class PhoneBookApplication:
         print("0 exit")
         print("1 add number")
         print("2 search")
+        print("3 add address")
 
     def add_number(self):
         name = input("name: ")
         number = input("number: ")
         self.__phonebook.add_number(name, number)
 
+    def add_address(self):
+        name = input("name: ")
+        address = input("address: ")
+        self.__phonebook.add_address(name, address)
+
     def search(self):
         name = input("name: ")
-        numbers = self.__phonebook.get_entry(name)
-        if numbers == None:
-            print("number unknown") 
-            return 
-        for number in numbers:
-            print(number)       
+        person = self.__phonebook.get_entry(name)
+        if person is None:
+            print("number unknown")
+        else:
+            numbers = person.numbers()
+            if not numbers:
+                print("number unknown")
+            else:
+                print(numbers)
+            if person.address():
+                print(person.address())
+            else:
+                print("address unknown")
+        if person is None:
+            print("address unknown")
 
     def execute(self):
         self.help()
@@ -52,6 +89,8 @@ class PhoneBookApplication:
                 self.add_number()
             elif command == "2":
                 self.search()
+            elif command == "3":
+                self.add_address()
             else:
                 self.help()
 
--- part10-12_course_records/src/course_records.py
+++ part10-12_course_records/src/course_records.py
@@ -1,1 +1,78 @@
-# tee ratkaisusi tänne
+class StudyTracker:
+    def __init__(self):
+        self.__courses = {}
+
+    def add_course(self, course: str, grade: int, credits: int):
+        if course not in self.__courses:
+            self.__courses[course] = {'grade': grade, 'credits': credits}
+        else:
+            # Update grade only if the new grade is higher
+            if grade > self.__courses[course]['grade']:
+                self.__courses[course]['grade'] = grade
+
+    def get_course_data(self, course: str):
+        if course in self.__courses:
+            return f"{course} ({self.__courses[course]['credits']} cr) grade {self.__courses[course]['grade']}"
+        else:
+            return "no entry for this course"
+
+    def get_statistics(self):
+        total_courses = len(self.__courses)
+        total_credits = sum(course['credits'] for course in self.__courses.values())
+        mean_grade = sum(course['grade'] for course in self.__courses.values()) / total_courses if total_courses > 0 else 0
+
+        grade_distribution = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+        for course in self.__courses.values():
+            grade_distribution[course['grade']] += 1
+
+        statistics = f"{total_courses} completed courses, a total of {total_credits} credits\nmean {mean_grade:.1f}\ngrade distribution"
+        for grade, count in grade_distribution.items():
+            if count > 0:
+                statistics += f"\n{grade}: {'x' * count}" if grade != 3 else f"\n{grade}:"
+        return statistics
+
+class StudyTrackerApplication:
+    def __init__(self):
+        self.__study_tracker = StudyTracker()
+
+    def help(self):
+        print("commands: ")
+        print("0 exit")
+        print("1 add course")
+        print("2 get course data")
+        print("3 statistics")
+
+    def add_course(self):
+        course = input("course: ")
+        grade = int(input("grade: "))
+        credits = int(input("credits: "))
+        self.__study_tracker.add_course(course, grade, credits)
+
+    def get_course_data(self):
+        course = input("course: ")
+        data = self.__study_tracker.get_course_data(course)
+        print(data)
+
+    def get_statistics(self):
+        statistics = self.__study_tracker.get_statistics()
+        print(statistics)
+
+    def execute(self):
+        self.help()
+        while True:
+            print("")
+            command = input("command: ")
+            if command == "0":
+                break
+            elif command == "1":
+                self.add_course()
+            elif command == "2":
+                self.get_course_data()
+            elif command == "3":
+                self.get_statistics()
+            else:
+                self.help()
+
+# when testing, no code should be outside application except the following:
+application = StudyTrackerApplication()
+application.execute()
