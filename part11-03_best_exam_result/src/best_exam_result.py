b5b292611712dedf87eb6ed7ed0a8bba5848327e
Riabokin Ruslan <rriabokin@gmail.com>
2024-03-12 15:42:11 +0200

    Часть 11-07  Список понятий

--- part11-03_best_exam_result/src/best_exam_result.py
+++ part11-03_best_exam_result/src/best_exam_result.py
@@ -6,6 +6,7 @@ class ExamResult:
         self.grade2 = grade2
         self.grade3 = grade3
 
-    def __str__(self):
-        return (f'Name:{self.name}, grade1: {self.grade1}' +
-            f', grade2: {self.grade2}, grade3: {self.grade3}')
+def best_results(results: list):
+    return [max([result.grade1, result.grade2, result.grade3]) for result in results]
+
+    
--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,11 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week_number: int, numbers: list):
+        self.week_number = week_number
+        self.numbers = numbers
+
+    def number_of_hits(self, my_numbers: list):
+        return sum([1 for number in my_numbers if number in self.numbers])
+
+    def hits_in_place(self, my_numbers: list):
+        return [number if number in self.numbers else -1 for number in my_numbers]
