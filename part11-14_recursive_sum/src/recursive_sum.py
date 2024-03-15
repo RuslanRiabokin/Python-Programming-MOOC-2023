22149384df25e52654d1054511e2588d285ef502
Riabokin Ruslan <rriabokin@gmail.com>
2024-03-15 14:35:30 +0200

    Часть 11-15 Рекурсия

--- part11-14_recursive_sum/src/recursive_sum.py
+++ part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,10 @@
 # WRITE YOUR SOLUTION HERE:
+def recursive_sum(number: int):
+    # if the number is 1, there is nothing else to add
+    if number <= 1:
+        return number
+
+    # fill in the rest of the function
+    # 1 + 2 + 3 + ... + n = n + recursive_sum(n - 1)
+    return number + recursive_sum(number - 1)
+
--- part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,33 @@
-
 def balanced_brackets(my_string: str):
-    if len(my_string) == 0:
-        return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
+    def is_valid_bracket(char):
+        return char in '()[]'
+
+    def is_matching_pair(opening, closing):
+        return (opening == '(' and closing == ')') or (opening == '[' and closing == ']')
+
+    def is_balanced_helper(sub_string):
+        stack = []
+        for char in sub_string:
+            if not is_valid_bracket(char):
+                continue
+            if char in '([':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if not is_matching_pair(top, char):
+                    return False
+        return not stack
+
+    # Call is_balanced_helper recursively on each substring
+    def balanced_brackets_recursive(my_string):
+        if len(my_string) == 0:
+            return True
+        for i in range(len(my_string)):
+            if is_valid_bracket(my_string[i]):
+                if is_balanced_helper(my_string[:i+1]) and balanced_brackets_recursive(my_string[i+1:]):
+                    return True
         return False
 
-    # remove first and last character
-    return balanced_brackets(my_string[1:-1])
+    return balanced_brackets_recursive(my_string)
