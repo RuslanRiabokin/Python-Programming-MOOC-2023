91736a3a8a7e9f3d0a3bf9b859bdbc55f10ad3c0
Riabokin Ruslan <rriabokin@gmail.com>
2024-03-21 17:13:10 +0200

    Часть 12-14 Обычные выражения

--- part12-14_regular_expressions/src/regular_expressions.py
+++ part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,14 @@
 # Write your solution here
 import re
+
+def is_dotw(my_string: str) -> bool:
+    pattern = r'\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
+    return bool(re.search(pattern, my_string))
+
+def all_vowels(my_string: str) -> bool:
+    pattern = r'^[aeiouAEIOU]+$'
+    return bool(re.match(pattern, my_string))
+
+def time_of_day(my_string: str) -> bool:
+    pattern = r'^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'
+    return bool(re.match(pattern, my_string))
